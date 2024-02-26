d2e6e200276ef282ca8871a6b01db88cb0031176
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-02-26 23:28:36 +0100

    Add files via upload

---
+++ testiamolabasedati/src/main/java/com/testiamolabasedati/testiamolabasedati/TestiamolabasedatiApplication.java
@@ -0,0 +1,13 @@
+package com.testiamolabasedati.testiamolabasedati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestiamolabasedatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestiamolabasedatiApplication.class, args);
+	}
+
+}
