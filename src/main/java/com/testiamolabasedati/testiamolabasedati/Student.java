89abefa6c18f28bf79885586485e76f10d748b70
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-02-28 12:35:10 +0100

    esercizio caricato

---
+++ src/main/java/com/testiamolabasedati/testiamolabasedati/Student.java
@@ -0,0 +1,61 @@
+package com.testiamolabasedati.testiamolabasedati;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+@Entity
+@Table
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    public Student(Long id, String lastName, String firstName, String email) {
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
